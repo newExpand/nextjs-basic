899138d1cb4c7f67c987aae43424036ce69ca7d2
newExpand <magin0auto@gmail.com>
2023-05-01 18:31:34 +0900

    Image 태그를 사용하여 최적

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Cart() {
-    return <div>장바구니입니다</div>;
+    return (
+        <div>
+            <h4 className="text-center">Cart</h4>
+            <div className="flex justify-around border-b-gray-500 leading-[0] p-2">
+                <p>상품명</p>
+                <p>$40</p>
+                <p>1개</p>
+            </div>
+            <div className="flex justify-around border-b-gray-500 leading-[0] p-2">
+                <p>상품명</p>
+                <p>$40</p>
+                <p>1개</p>
+            </div>
+        </div>
+    );
 }
--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,14 +1,25 @@
+"use client";
+
+import Image from "next/image";
+
 export default function List() {
     let products = ["Tomatoes", "Pasta", "Coconut"];
 
     return (
         <div>
-            <h4 className="text-center mt-[50vh]">상품목록</h4>
+            <h4 className="mt-20 text-center">상품목록</h4>
             {products.map((item, index) => (
                 <div
                     key={item}
-                    className="w-48 p-5 mx-auto mt-5 text-black bg-white rounded-md"
+                    className="relative w-48 p-5 mx-auto mt-5 text-black bg-white rounded-md"
                 >
+                    <Image
+                        src={`/food${index}.png`}
+                        width={50}
+                        height={50}
+                        className="w-full h-auto"
+                        alt=""
+                    />
                     <h4>{`${item} ${index + 1}`}만원</h4>
                 </div>
             ))}
