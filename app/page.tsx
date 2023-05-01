95c215e357d74fb3a381372e40e18f87333892ee
newExpand <magin0auto@gmail.com>
2023-05-01 16:19:18 +0900

    react 기초문법 정리 및 tailwind css 적용 - 변수 선언 및 적용 - 인라인 스타일 적용

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,19 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    return <div></div>;
+    let name = "Lee";
+    let link = "http://google.com";
+    return (
+        <div>
+            <h4 className="text-center mt-[50vh]">SM프레시</h4>
+            <p className="text-center">by dev {name}</p>
+            <a
+                className="block text-center"
+                style={{ color: "red", fontSize: "30px" }}
+                href={link}
+            >
+                링크
+            </a>
+        </div>
+    );
 }
